6dbd3f4a722c6e6a7756aa428f339f78048ce69f
Bart <kluczynskib@gmail.com>
2017-10-27 14:33:14 +0100

    added basic css to divide myPlaces and a map;created Location class to hold title and location;added ViewModel holding locationsList(an observable)

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const Location = function(data){
+  this.title = data.title;
+  this.location = data.location;
+}
+
+let myLocations = [
+          {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+          {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
+          {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
+          {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
+          {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
+          {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+];
+
+
+const ViewModel = function(){
+  const self = this;
+  self.locationsList = ko.observableArray([]);
+  myLocations.forEach(function(location){
+    self.locationsList.push( new Location(location))
+  })
+
+}
+
+ko.applyBindings(new ViewModel())
