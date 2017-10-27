759b6a5e26725bf1735d8d97bb4ff58358cd3d75
Bart <kluczynskib@gmail.com>
2017-10-27 16:30:26 +0100

    added uuid for marker id;added initMap callback to load up the map and markers;

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
+//class of Location initialized with title and location
 const Location = function(data){
   this.title = data.title;
   this.location = data.location;
 }
 
+const Markerito = function(data, map){
+  this.map = map,
+  this.position = data.location,
+  this.title = data.title,
+  this.animation = google.maps.Animation.DROP,
+  this.id = UUID.generate()
+}
+
+//hardcoded Locations
 let myLocations = [
           {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
           {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
@@ -12,14 +22,36 @@ let myLocations = [
           {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
 ];
 
+function initMap(){
+  const markers = [];
+  let map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 40.7413549, lng: -73.9980244},
+          zoom: 13
+        });
+  let largeInfoWindow = new google.maps.InfoWindow();
+  let bounds = new google.maps.LatLngBounds();
+    for (let i = 0; i < myLocations.length; i++){
+      markers.push(new google.maps.Marker(
+        new Markerito(myLocations[i], map)));
+    }
+  console.log(markers)
+}
 
+//ViewModel
 const ViewModel = function(){
+
   const self = this;
   self.locationsList = ko.observableArray([]);
+
   myLocations.forEach(function(location){
     self.locationsList.push( new Location(location))
   })
 
+  self.selectLocation = function(){
+    console.log("i got clicked")
+  }
+
 }
+
 
 ko.applyBindings(new ViewModel())
