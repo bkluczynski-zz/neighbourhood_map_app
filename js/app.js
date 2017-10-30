53c87a20f8d8339bf045ed7de26d8a9548f951dd
Bart <kluczynskib@gmail.com>
2017-10-30 09:55:14 +0000

    added comments to explain what every functions does

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const Location = function(data) {
     this.location = data.location;
 }
 
+//class of Marker
 const Markerito = function(data, map) {
     this.map = map,
     this.position = data.location,
@@ -13,7 +14,7 @@ const Markerito = function(data, map) {
 }
 
 let map;
-//hardcoded Locations
+//hardcoded Locations - can be changed into API to an external DB
 let myLocations = [
     {
         title: 'Arcola Theatre',
@@ -53,7 +54,7 @@ let myLocations = [
         }
     }
 ];
-
+//map initialization
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
@@ -69,7 +70,6 @@ const ViewModel = function() {
 
     const self = this;
     const baseUrl = "http://en.wikipedia.org/w/api.php?&origin=*&action=query&prop=extracts&format=json&exintro=&titles=";
-
     let bounds;
     let marker;
     let largeInfoWindow;
@@ -80,6 +80,7 @@ const ViewModel = function() {
         self.putMarkersDown();
         self.initializeLargeInfoAndBounds();
     } else {
+      //if it's not defined wait
         setTimeout(function() {
             self.putLocations();
             self.putMarkersDown();
@@ -87,19 +88,23 @@ const ViewModel = function() {
         }, 500)
     }
 
+    //markers list being an observable array
     self.markers = ko.observableArray([])
+    //location list being an observable array
     self.locationsList = ko.observableArray([]);
+    //search input being an observable
     self.searchLocation = ko.observable('');
 
-
+    //display myLocations in the list
     self.putLocations = function() {
         myLocations.forEach(function(location) {
             self.locationsList.push(new Location(location))
         })
     }
-
+    //make a marker bounce when selected either on the list or directly by clicking on the marker
     self.toggleBounce = function(marker){
       self.markers().forEach(function(item){
+        //before making a marker bounce make sure the rest of markers don't bounce
         if (item.getAnimation() !== null) {
           item.setAnimation(null);
         }
@@ -107,7 +112,7 @@ const ViewModel = function() {
           marker.setAnimation(google.maps.Animation.BOUNCE);
         }
 
-
+    //display markers on the map
     self.putMarkersDown = function() {
         self.locationsList().forEach(function(location) {
             marker = new google.maps.Marker(new Markerito(location, map))
@@ -118,6 +123,7 @@ const ViewModel = function() {
         })
     }
 
+    //filter markers having a part of their location.title matching filter query
     self.filterMarkers = function() {
         for (let i = 0; i < self.markers().length; i++) {
             if ((self.markers()[i].title.toLowerCase().indexOf(self.searchLocation().toLowerCase()) > -1) || (self.searchLocation().length === 0)) {
@@ -127,13 +133,15 @@ const ViewModel = function() {
             }
         }
     }
-
+    //get JSON and parse it into extract and id
     self.parseJsonAndDisplay = function(data, innerHTML, location) {
         let dataObj = data.query.pages;
         let id = Object.keys(dataObj).join();
+        //in case dataObj is undefined (when wiki does not find any info about location) output the generic message
         let extract = dataObj[id].extract === undefined
             ? ' Sorry, there is no available results for this place in Wikipedia'
             : dataObj[id].extract;
+        //displayHTMLOfExtract
         self.displayHTML(innerHTML, dataObj, id, extract, location);
     }
 
@@ -141,33 +149,40 @@ const ViewModel = function() {
         innerHTML += '<strong>' + dataObj[id].title + '</strong>';
         innerHTML += extract;
         innerHTML += '<em> credit to Wikipedia</em>';
+        //display infoWindow
         self.displayInfoWindow(innerHTML,location)
     }
 
+    //initialize largeInfoWindow and bounds
     self.initializeLargeInfoAndBounds = function() {
         largeInfoWindow = new google.maps.InfoWindow();
         bounds = new google.maps.LatLngBounds();
     }
 
+    //set content of infoWindow to whatever HTML is passed
     self.displayInfoWindow = function(innerHTML, location){
       largeInfoWindow.setContent(innerHTML)
       largeInfoWindow.open(map, location)
     }
 
     self.populateInfoWindow = function(marker, target) {
+        //get the real marker
         marker = self.markers().filter(markerito => markerito.title === marker.title)[0];
+        //if infoWindow does not have a marker property
         if (largeInfoWindow.marker != marker) {
             largeInfoWindow.marker = marker;
+            //animate the marker
             self.toggleBounce(marker)
+            //open infoWindow with Wikipedia extract
             self.openInfoBox(marker)
-            // Make sure the marker property is cleared if the infowindow is closed.
+            // Make sure the marker and animation property is cleared if the infowindow is closed.
             largeInfoWindow.addListener('closeclick', function() {
                 largeInfoWindow.marker = null;
                 marker.setAnimation(null);
             });
         }
     }
-
+    //handle error if request fails for any reason
     self.handleError = function(location, innerHTML, err) {
         innerHTML += '<strong>' + location.title + '</strong>' + " Could not get description from Wikipedia. Try again later"
         innerHTML += err ? 'error code: ' + err : '';
@@ -196,6 +211,8 @@ const ViewModel = function() {
             self.handleError(location, innerHTML, err)
         })
     }
+
+    //filter locations in the list using ko.computed and utils.arrayFilter
     self.filteredLocations = ko.computed(function() {
         return ko.utils.arrayFilter(self.locationsList(), function(result) {
             self.filterMarkers(result)
